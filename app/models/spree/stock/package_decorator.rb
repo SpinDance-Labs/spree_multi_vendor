5e3fda2ab66145c72c7fd14c9cc443b091c570da
Chris Samuelson <chris@spindancelabs.com>
2020-12-20 22:07:13 -0500

    fix shipping method issues

--- app/models/spree/stock/package_decorator.rb
+++ app/models/spree/stock/package_decorator.rb
@@ -2,7 +2,7 @@ module Spree::Stock::PackageDecorator
   def shipping_methods
     vendor = stock_location.vendor
     
-    if vendor && Spree::ShippingMethod.method_defined?(:vendor)
+    if vendor && Spree::ShippingMethod.method_defined?(:vendor) && vendor.shipping_methods.count > 0
       vendor.shipping_methods.to_a
     else
       shipping_categories.map(&:shipping_methods).reduce(:&).to_a
